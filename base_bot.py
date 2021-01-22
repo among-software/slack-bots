db3b2b13eeb194b29ab85158bafac38ea1d768a0
zaeval <zaeval@kookmin.ac.kr>
2021-01-22 11:40:23 +0900

    first init

---
+++ base_bot.py
@@ -0,0 +1,35 @@
+import requests
+from pandas.io.json import json_normalize
+
+
+def channelId(channel_name, slack_token):
+    # 채널 조회 API 메소드: conversations.list
+    URL = 'https://slack.com/api/conversations.list'
+
+    # 파라미터
+    params = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'token': slack_token
+    }
+
+    # API 호출
+    res = requests.get(URL, params=params)
+
+    channel_list = json_normalize(res.json()['channels'])
+    channel_id = list(channel_list.loc[channel_list['name'] == channel_name, 'id'])[0]
+
+    return {'channel_name': channel_name, 'channel_id': channel_id}
+
+
+def sendMessage(channel_id, message, slack_token):
+    # 파라미터
+    data = {'Content-Type': 'application/x-www-form-urlencoded',
+            'token': slack_token,
+            'channel': channel_id,
+            'text': message,
+            'reply_broadcast': 'True',
+            }
+
+    # 메시지 등록 API 메소드: chat.postMessage
+    URL = "https://slack.com/api/chat.postMessage"
+    res = requests.post(URL, data=data)
---
+++ clean-bot.py
@@ -0,0 +1,11 @@
+import random
+import sys
+import base_bot
+
+slack_token = sys.argv[0]
+channelName = "공지"
+RANDOM_TAIL = ["있답니다.","있어요 ㅎㅎ.","있으니, 도망가지마세요.","... 이쯤 되면 다들 아시죠?","있습니다."]
+message = "[공지봇] 금일 18:30에 청소가 "+random.choice(RANDOM_TAIL)
+
+channel_id = base_bot.channelId(channelName, slack_token)
+base_bot.sendMessage(channel_id,message,slack_token)
